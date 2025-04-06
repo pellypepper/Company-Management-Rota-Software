129710328e97ee1ae87b5f00b97a2e52e1dacd24
ppeliance <144333180+ppeliance@users.noreply.github.com>
2025-04-06 13:32:18 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,19 +3,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 
+const connectionString = process.env.DATABASE_URL;
+
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432,
-
-  ssl: {
-    rejectUnauthorized: false // Required for RDS connections
-  }
-
+  connectionString: connectionString,
+  rejectUnauthorized: false 
 }
+
 );
 
 pool.connect()
